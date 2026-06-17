4c8cda619786fc2dd4df255a6184e593a62cf994
jvacero <johnvictorgacero@gmail.com>
2026-06-17 10:08:33 +0800

    Added input.php for php scripting

--- register.php
+++ register.php
@@ -1,12 +1,15 @@
 <html>
 <head>
 <title>
+Student Information Form - Acero
 </title>
 </head>
 
 <body>
 <form action="input.php" method="POST">
-	Name: <input type="text" name="username"
+	Name: <input type="text" name="username">
+	Address: <input type="text" name="">
+	
 </form>
 </body>
 </html>
