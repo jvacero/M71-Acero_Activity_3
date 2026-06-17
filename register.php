6c98eb1c213df565ce42067e362369ee95e0545f
jvacero <johnvictorgacero@gmail.com>
2026-06-18 01:36:38 +0800

    Added Style.css but not yet used

--- register.php
+++ register.php
@@ -7,9 +7,16 @@ Student Information Form - Acero
 
 <body>
 <form action="input.php" method="POST">
-	Name: <input type="text" name="username">
-	Address: <input type="text" name="">
-	
+	Last Name: <input type="text" name="lastName">
+	Middle Name: <input type="text" name="midName">
+	First Name: <input type="text" name="firstName">
+	Address: <input type="text" name="addHome">
+	Contact Number: <input type="text" name="phoneNum">
+	Birthplace: <input type="text" name="birthPlace">
+	Birthday: <input type="date" name="birthDay">
+	Religion: <input type="text" name="religion">
+	Civil Status: <input type="radio" name="status">
+	Nationality: <input type="text" name="ntnLity">
 </form>
 </body>
 </html>
