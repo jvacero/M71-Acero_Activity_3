38e0ba558e59fb8044cc3d65900884570ea7e9fe
jvacero <johnvictorgacero@gmail.com>
2026-06-18 11:47:36 +0800

    Updated style

--- register.php
+++ register.php
@@ -10,16 +10,20 @@ Student Information Form - Acero
 <body>
 	<p>Registration Form</p>
 <form class="forminput" action="input.php" method="POST">
-	Last Name: <input type="text" placeholder="Last Name" name="lastName">
-	Middle Name: <input type="text" placeholder="Middle Name" name="midName">
-	First Name: <input type="text" placeholder="First Name" name="firstName">
-	Address: <input type="text" placeholder="Address" name="addHome">
-	Contact Number: <input type="text" placeholder="Contact Number" name="phoneNum">
-	Birthplace: <input type="text" placeholder="Birthplace" name="birthPlace">
-	Birthday: <input type="date" placeholder="Birthday" name="birthDay">
-	Religion: <input type="text" placeholder="Religion" name="religion">
-	Civil Status: <input id="civStat" type="radio" name="status">
-	Nationality: <input placeholder="Nationality" type="text" name="ntnLity">
+	<div>	
+	<input type="text" placeholder="Last Name" name="lastName">
+	<input type="text" placeholder="Middle Name" name="midName">
+	<input type="text" placeholder="First Name" name="firstName">
+	<input type="text" placeholder="Address" name="addHome">
+	<input type="text" placeholder="Contact Number" name="phoneNum">
+	</div>
+	<div>
+	<input type="text" placeholder="Birthplace" name="birthPlace">
+	<input type="date" placeholder="Birthday" name="birthDay">
+	<input type="text" placeholder="Religion" name="religion">
+	<input class="radioBtn" id="civStat" type="radio" name="status">
+	<input placeholder="Nationality" type="text" name="ntnLity">
+	</div>
 </form>
 </body>
 </html>
