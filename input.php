66fdec919c080f6c508160820e3e5bdb7ebd4927
jvacero <johnvictorgacero@gmail.com>
2026-06-24 02:22:11 +0800

    - Fixed web responsiveness 3 modes available square, wide, phone.  - Fixed appearance (style.css)

--- input.php
+++ input.php
@@ -1,5 +1,19 @@
 <?php
-
-
-
+    $lastNm = $_POST["lastName"];
+    $midNm = $_POST["midName"];
+    $firstNm = $_POST["firstName"];
+    $address = $_POST["addHome"];
+    $num = $_POST["phoneNum"];
+    $bPlace = $_POST["birthPlace"];
+    $bDay = $_POST["birthDay"];
+    $rel = $_POST["religion"];
+    $stat = $_POST["status"];
+    $nation = $_POST["nanLity"];
+    echo "<p>Name: $firstNm $midNm $lastNm</p>";
+    echo "Address: $address";
+    echo "Contact Number: $num";
+    echo "Birth Place: $bPlace";
+    echo "Religion: $rel";
+    echo "Status: $stat";
+    echo "Nationality: $nation";
 ?>
--- register.php
+++ register.php
@@ -8,26 +8,33 @@ Student Information Form - Acero
 </head>
 
 <body>
-	<p>Registration Form</p>
+	<header>
+		<p>Registration Form</p>
+	</header>
+
 <form class="forminput" action="input.php" method="POST">
 	<div>	
-	<input type="text" placeholder="Last Name" name="lastName">
-	<input type="text" placeholder="Middle Name" name="midName">
-	<input type="text" placeholder="First Name" name="firstName">
-	<input type="text" placeholder="Address" name="addHome">
-	<input type="text" placeholder="Contact Number" name="phoneNum">
+		<input type="text" placeholder="Last Name" name="lastName">
+		<input type="text" placeholder="Middle Name" name="midName">
+		<input type="text" placeholder="First Name" name="firstName">
+		<input type="text" placeholder="Address" name="addHome">
+		<input type="text" placeholder="Contact Number" name="phoneNum">
 	</div>
 	<div>
 	<input type="text" placeholder="Birthplace" name="birthPlace">
 	<input type="date" placeholder="Birthday" name="birthDay">
 	<input type="text" placeholder="Religion" name="religion">
-	<input class="radioBtn" id="civStat" type="radio" name="status">
+	<p class="civstatus">Civil Status</p>
+	<div class="radioBtn">
+		<input value="single" type="radio" name="status" required>
+		<label for="single">Single</label>
+		<input value="married" type="radio" name="status">
+		<label for="married">Married</label>
+	</div>
 	<input placeholder="Nationality" type="text" name="ntnLity">
-</div>
-<div class="submitBtn">
-<button>test</button>
+	
+	<button class="submitBtn">DONE</button>
 </div>
 </form>
-
 </body>
 </html>
