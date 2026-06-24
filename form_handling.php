8fc7dd968637a125b0207a4db32c10cb2dc08ed2
jvacero <johnvictorgacero@gmail.com>
2026-06-24 21:40:50 +0800

    Added form handling UI and a button that will return the user to register.php

--- form_handling.php
+++ form_handling.php
@@ -1,4 +1,17 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+<body>
+
+
+<div class="form_done">
+    <?php
     $lastNm = $_POST["lastName"];
     $midNm = $_POST["midName"];
     $firstNm = $_POST["firstName"];
@@ -8,12 +21,18 @@
     $bDay = $_POST["birthDay"];
     $rel = $_POST["religion"];
     $stat = $_POST["status"];
-    $nation = $_POST["nanLity"];
-    echo "<p>Name: $firstNm $midNm $lastNm</p>";
-    echo "Address: $address";
-    echo "Contact Number: $num";
-    echo "Birth Place: $bPlace";
-    echo "Religion: $rel";
-    echo "Status: $stat";
-    echo "Nationality: $nation";
-?>
+    $nation = $_POST["ntnLity"];
+    echo "<h4>Name: $firstNm $midNm $lastNm</h4>";
+    echo "<h4>Address: $address</h4>";
+    echo "<h4>Contact Number: $num</h4>";
+    echo "<h4>Birth Date: $bDay</h4>";
+    echo "<h4>Birth Place: $bPlace</h4>";
+    echo "<h4>Religion: $rel</h4>";
+    echo "<h4>Status: $stat</h4>";
+    echo "<h4>Nationality: $nation</h4>";
+    ?>
+    <button class="return"><a href="register.php">return</a></button>
+</div>
+    
+</body>
+</html>
