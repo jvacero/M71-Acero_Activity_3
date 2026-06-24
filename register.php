ce09af329bb3ca4e3e6299f543699eefe9e0d568
jvacero <johnvictorgacero@gmail.com>
2026-06-24 21:39:49 +0800

    Added button hover event, fixed radio button alignment.

--- register.php
+++ register.php
@@ -12,26 +12,26 @@ Student Information Form - Acero
 		<p>Registration Form</p>
 	</header>
 
-<form class="forminput" action="input.php" method="POST">
+<form class="forminput" action="form_handling.php" method="POST">
 	<div>	
-		<input type="text" placeholder="Last Name" name="lastName">
-		<input type="text" placeholder="Middle Name" name="midName">
-		<input type="text" placeholder="First Name" name="firstName">
-		<input type="text" placeholder="Address" name="addHome">
-		<input type="text" placeholder="Contact Number" name="phoneNum">
+		<input type="text" placeholder="Last Name" name="lastName" required>
+		<input type="text" placeholder="Middle Name" name="midName" >
+		<input type="text" placeholder="First Name" name="firstName" required>
+		<input type="text" placeholder="Address" name="addHome" required>
+		<input type="text" placeholder="Contact Number" name="phoneNum" required>
 	</div>
 	<div>
-	<input type="text" placeholder="Birthplace" name="birthPlace">
-	<input type="date" placeholder="Birthday" name="birthDay">
-	<input type="text" placeholder="Religion" name="religion">
+	<input type="text" placeholder="Birthplace" name="birthPlace" required>
+	<input type="date" placeholder="Birthday" name="birthDay" required>
+	<input type="text" placeholder="Religion" name="religion" required>
 	<p class="civstatus">Civil Status</p>
 	<div class="radioBtn">
-		<input value="single" type="radio" name="status" required>
-		<label for="single">Single</label>
-		<input value="married" type="radio" name="status">
-		<label for="married">Married</label>
+		<input value="Single" type="radio" name="status" required>
+		<label for="Single">Single</label>
+		<input value="Married" type="radio" name="status">
+		<label for="Married">Married</label>
 	</div>
-	<input placeholder="Nationality" type="text" name="ntnLity">
+	<input placeholder="Nationality" type="text" name="ntnLity" required>
 	
 	<button class="submitBtn">DONE</button>
 </div>
